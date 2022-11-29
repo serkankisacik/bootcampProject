e8481dd1ded7cda8cbb15f6e51cc3b404c81edff
Serkan Kisacik <serkankisacik01@gmail.com>
2022-11-29 03:18:34 +0300

    Validation Password & email

---
+++ src/main/java/com/kodlamaio/bootcampproject/business/constants/MessagesValidation.java
@@ -0,0 +1,11 @@
+package com.kodlamaio.bootcampproject.business.constants;
+
+public class MessagesValidation {
+
+    public static final String NationalIdNotNull = "National number cannot be empty";
+    public static final String NationalIdTooLong = "National number too long";
+    public static final String NationalIdTooShort = "National number too short";
+
+    public static final String PasswordMessage = "Password must be minimum 8 characters, at least one letter, one number and one special character:";
+    public static final String NameTooShort = "Name too short. The name must be at least 3 characters.";
+}
--- src/main/java/com/kodlamaio/bootcampproject/business/requests/users/CreateApplicantRequest.java
+++ src/main/java/com/kodlamaio/bootcampproject/business/requests/users/CreateApplicantRequest.java
@@ -1,11 +1,11 @@
 package com.kodlamaio.bootcampproject.business.requests.users;
 
+import com.kodlamaio.bootcampproject.business.constants.MessagesValidation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
@@ -13,12 +13,22 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class CreateApplicantRequest {
     @NotBlank(message = "Ad boş olamaz")
+    @Min(value = 3, message = MessagesValidation.NameTooShort)
     private String firstName;
 
     private String lastName;
+
+    @Email(regexp = ".+[@].+[\\.].+", message = "Email must be valid")
     private String email;
+
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$", message = MessagesValidation.PasswordMessage)
     private String password;
+
     private String about;
     private LocalDate dateOfBirth;
+
+    @NotNull(message = MessagesValidation.NationalIdNotNull)
+    @Max(value = 11, message = MessagesValidation.NationalIdTooLong)
+    @Min(value = 11, message = MessagesValidation.NationalIdTooShort)
     private String nationalId;
 }
